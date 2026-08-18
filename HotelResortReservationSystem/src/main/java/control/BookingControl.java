07e4f065ff3408c556003e36c7d7f3b23b160cd3
tanghy-404 <tanghy-wm23@student.tarc.edu.my>
2026-08-18 15:14:57 +0800

    Update BookingControl.java

--- HotelResortReservationSystem/src/main/java/control/BookingControl.java
+++ HotelResortReservationSystem/src/main/java/control/BookingControl.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+* Author: Tang Hong Yi
+ * Handling booking process, manage guest booking, mange guest queue
  */
 package control;
 
